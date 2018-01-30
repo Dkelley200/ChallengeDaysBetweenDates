92573948ab34edac2129b25d80587882d1a953b3
DKelley <dianna.kelley@gmail.com>
2018-01-29 20:11:06 -0800

    Add project files.

---
+++ ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstdate = Calendar1.SelectedDate.Date;
+            DateTime seconddate = Calendar2.SelectedDate.Date;
+            if  ( firstdate < seconddate)
+                {
+                TimeSpan daterange = ( seconddate - firstdate);
+                resultLabel.Text = daterange.TotalDays.ToString();
+                }
+            else
+                {
+               TimeSpan daterange = ( firstdate - seconddate);
+                resultLabel.Text = daterange.TotalDays.ToString();
+            }
+
+            
+
+           
+        }
+    }
+}
